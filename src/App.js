d6d476f4a858820d0d45d110e9374991bcd49c46
rajat-mehra05 <rajat.m.5278@gmail.com>
2022-01-10 23:10:13 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import Footer from './components/Footer';
 import InputField from './components/InputField';
 import Keypad from './components/Keypad';
 
@@ -10,6 +11,7 @@ function App() {
     <div className='flex justify-center items-center min-h-screen flex-col'>
       <InputField text={text} setText={setText} />
       <Keypad text={text} setText={setText} />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='fixed bottom-0 mb-4'>
+            <p className="font-semibold"> 
+            Refer to <a className='text-red-900' href="https://github.com/rajat-mehra05/t9-keyboard-react#readme"> Readme section </a> for any reference.
+             </p>
+        </div>
+    )
+}
+
+export default Footer
