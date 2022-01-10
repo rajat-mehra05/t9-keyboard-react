4cd07893baed95a44249128dcf953ec4826a0875
rajat-mehra05 <rajat.m.5278@gmail.com>
2022-01-10 22:48:45 +0530

    fix: input field readonly

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -2,13 +2,14 @@ const InputField = (props) => {
 
     return (
         <div>
-            <input
+         <input
             type="text" 
+            readOnly
             value={props.text}
             onChange={(e) => props.setText(e.target.value)}
             className='border-black border-2 rounded p-2 w-72 sm:w-96 mb-4' 
             placeholder='Type here...' />
-        </div>
+         </div>
     )
 }
 
