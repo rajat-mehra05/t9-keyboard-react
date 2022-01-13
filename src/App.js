d884f486a4908abd26dd5d4cbe0adc76ac5614e9
rajat-mehra05 <rajat.m.5278@gmail.com>
2022-01-13 10:12:32 +0530

    feat: delete btn added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Footer from './components/Footer';
 import InputField from './components/InputField';
 import Keypad from './components/Keypad';
+import DeleteButton from './components/DeleteButton';
 
 function App() {
   const [text, setText] = useState("");
@@ -11,6 +12,7 @@ function App() {
     <div className='flex justify-center items-center min-h-screen flex-col'>
       <InputField text={text} setText={setText} />
       <Keypad text={text} setText={setText} />
+      <DeleteButton setText={setText} />
       <Footer />
     </div>
   );
---
+++ src/components/DeleteButton.jsx
@@ -0,0 +1,16 @@
+const DeleteButton = (props) => {
+
+    const handleDelete = () => {
+        props.setText((prev) =>  prev.slice(0,-1))
+    }
+
+    return (
+        <button 
+        onClick={handleDelete}
+        className="bg-zinc-900 focus:bg-red-600 flex flex-col items-center rounded w-80 sm:w-96 m-2 p-2 text-white">
+            backspace    
+        </button>
+    )
+}
+
+export default DeleteButton
